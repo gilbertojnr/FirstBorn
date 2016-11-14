bccbc8c453b8fd11f90dfe15ca587fe8310f02a5
Gilbert Akpan <gilbertakpan@silver-surfer.apollosrc.root>
2016-11-14 11:27:58 -0600

    FirstBorn

---
+++ FirstBorn/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Person p = new Person();
+        p.name = "Alice";
+        p.age = 30;
+        p.isAlive = true;
+    }
+}
---
+++ FirstBorn/src/com/company/Person.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by gilbertakpan on 11/14/16.
+ */
+public class Person {
+    String name;
+    int age;
+    boolean isAlive;
+}
