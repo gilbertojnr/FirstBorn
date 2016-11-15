0171d54dbe07f6ca099fde503858a59da93df87c
Gilbert Akpan <gilbertakpan@silver-surfer.apollosrc.root>
2016-11-15 11:07:26 -0600

    Second day

--- FirstBorn/src/com/company/Main.java
+++ FirstBorn/src/com/company/Main.java
@@ -3,9 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	Person p = new Person();
-        p.name = "Alice";
-        p.age = 30;
-        p.isAlive = true;
+        Person.isValidName("Bob Johnson");
+
+        System.out.println(Person.hair);
     }
-}
+
+
+
+
+    }
+
